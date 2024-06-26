6af6c63df964d887d6264a70e5608fe39488d9ef
Suman Kumar <skumar21@vmware.com>
2024-06-26 11:52:11 +0530

    Adding a Basic controller

--- src/main/java/com/org/spring_auth/SpringAuthApplication.java
+++ src/main/java/com/org/spring_auth/SpringAuthApplication.java
@@ -2,6 +2,9 @@ package com.org.spring_auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class SpringAuthApplication {
@@ -10,4 +13,12 @@ public class SpringAuthApplication {
 		SpringApplication.run(SpringAuthApplication.class, args);
 	}
 
+}
+
+@RestController
+class BasicController {
+	@GetMapping("/hello")
+	public ResponseEntity<String> sayHello() {
+		return ResponseEntity.ok("Hello World");
+	}
 }
